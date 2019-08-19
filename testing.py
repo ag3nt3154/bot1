53169ed6cb40a35e048751ed12a655c3f08eec60
ag3nt3154 <ag3nt3154@gmail.com>
2019-08-19 13:40:11 +0800

    update

--- testing.py
+++ testing.py
@@ -1,3 +1,9 @@
 # import pandas as pd
 # data = pd.read_csv('emotion_training.csv')
 
+import openpyxl
+
+path = 'C:\\Users\\alexr\PycharmProjects\\bot1\\emotion_training.xlsx'
+
+wb = openpyxl.load_workbook(path)
+
