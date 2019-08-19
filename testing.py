bc2bd51f8a67a3c750b24e4cc6e23c35f46dfc59
ag3nt3154 <ag3nt3154@gmail.com>
2019-08-19 14:50:33 +0800

    update to use json

--- testing.py
+++ testing.py
@@ -1,9 +1,42 @@
-# import pandas as pd
-# data = pd.read_csv('emotion_training.csv')
+from misc_fn import dump, load
+list = load('emotion.json')
+print(list)
 
-import openpyxl
+training_dict = {
+    #  Keltner, Dacher; Cowen, Alan S. (2017-09-19).
+    #  "Self-report captures 27 distinct categories of emotion bridged by continuous gradients".
+    #  Proceedings of the National Academy of Sciences 114 (38): E7900–E7909.
+    #  doi:10.1073/pnas.1702247114. ISSN 0027-8424. PMID 28874542.
+    'emotions_list': [
+        'Admiration',
+        'Adoration',
+        'Aesthetic Appreciation',
+        'Amusement',
+        'Anxiety',
+        'Awe',
+        'Awkwardness',
+        'Boredom',
+        'Calmness',
+        'Confusion',
+        'Craving',
+        'Disgust',
+        'Empathetic pain',
+        'Entrancement',
+        'Envy',
+        'Excitement',
+        'Fear',
+        'Horror',
+        'Interest',
+        'Joy',
+        'Nostalgia',
+        'Romance',
+        'Sadness',
+        'Satisfaction',
+        'Sexual desire',
+        'Sympathy',
+        'Triumph'
+    ]
+    'emoticon_list': [
 
-path = 'C:\\Users\\alexr\PycharmProjects\\bot1\\emotion_training.xlsx'
-
-wb = openpyxl.load_workbook(path)
-
+    ]
+}
